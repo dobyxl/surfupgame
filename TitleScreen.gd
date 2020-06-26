a1da9ccf6d1b296070ff51bfac598b6124c7d4f6
dobyxl <67363941+dobyxl@users.noreply.github.com>
2020-06-25 20:07:13 -0600

    added title screen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -18,7 +18,9 @@ func _ready():
 
 func _on_Start_Game_pressed():
 	get_tree().change_scene("res://World.tscn")
+	pass # Replace with function body.
 
 
 func _on_Quit_Game_pressed():
 	get_tree().quit()
+	pass # Replace with function body.
